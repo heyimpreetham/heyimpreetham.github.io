a0d5076def47ac2533be533db59ba177e2b6ab4d
Preetham <50054134+preethampsg@users.noreply.github.com>
2020-06-24 18:46:04 +0530

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -16,7 +16,7 @@ $errors .= “\n Error: Invalid email address”;
 if( empty($errors))
 {
 $to = $myemail;
-$email_subject = “Contact form submission: $name”;
+$email_subject = “Portfolio Reviews: $name”;
 $email_body = “You have received a new message. “.
 ” Here are the details:\n Name: $name \n “.
 “Email: $email_address\n Message \n $message”;
@@ -24,6 +24,6 @@ $headers = “From: $myemail\n”;
 $headers .= “Reply-To: $email_address”;
 mail($to,$email_subject,$email_body,$headers);
 //redirect to the ‘thank you’ page
-header(‘Location: contact-form-thank-you.html’);
+header(‘Location: reviewsent.html’);
 }
 ?>
